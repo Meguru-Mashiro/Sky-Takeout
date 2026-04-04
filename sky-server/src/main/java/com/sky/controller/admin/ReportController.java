a450128b4ef52994cb2c8ec9676f027193e21ce4
Meguru <18759670056@163.com>
2026-04-04 09:26:10 +0800

    订单数据统计功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -2,6 +2,7 @@ package com.sky.controller.admin;
 
 import com.sky.result.Result;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -25,7 +26,7 @@ public class ReportController {
     private ReportService reportService;
     @GetMapping("/turnoverStatistics")
     @ApiOperation("营业额统计")
-    public Result<TurnoverReportVO> getTurnoverStatistics(
+    public Result<TurnoverReportVO> turnoverStatistics(
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         log.info("营业额数据统计：{},{}", begin, end);
@@ -33,10 +34,18 @@ public class ReportController {
     }
     @GetMapping("/userStatistics")
     @ApiOperation("用户统计")
-    public Result<UserReportVO> getUserStatistics(
+    public Result<UserReportVO> userStatistics(
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         log.info("用户数据统计：{},{}", begin, end);
         return Result.success(reportService.getUserStatistics(begin, end));
+    }
+    @GetMapping("/ordersStatistics")
+    @ApiOperation("订单统计")
+    public Result<OrderReportVO> ordersStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
+        log.info("订单数据统计：{},{}", begin, end);
+        return Result.success(reportService.getOrdersStatistics(begin, end));
     }
 }
